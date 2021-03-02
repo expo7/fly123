1f08d2b791ff527888a80dfbc0b1ebdfc2a6bc60
expo7 <bsavelli66@gmail.com>
2021-03-02 16:13:08 -0700

    commited

--- flask_qa/app.py
+++ flask_qa/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template,request
-from flask import Flask
+import robin_stocks as rs 
+def sign_in():
+    rs.robinhood.login(username='bsavelli66@gmail.com', password='Canyon6687', 
+                          expiresIn=86400, scope='internal', by_sms=True,
+                          store_session=True)
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def dashboard():
-    return 'hello'
+    sign_in()
+    order=rs.robinhood.order_buy_crypto_by_price('BTC',5)
+    return order 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
